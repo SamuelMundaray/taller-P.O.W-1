21f1e137d394d3919dfac6f0e4765c85ea65af15
Samuel Mundaray <semundaray.25@est.ucab.edu.ve>
2026-05-03 11:15:17 -0400

    se removio una funcion comentada

--- src/modules/Board_Games_Catalog/boardGamesService.js
+++ src/modules/Board_Games_Catalog/boardGamesService.js
@@ -50,23 +50,7 @@ boardGamesService.updateBoardGame = (id, name, minplayers, maxplayers, duration,
   return null;
 };
 
-/*boardGamesService.updateBoardGame = (id,new_name,new_minplayers,new_maxplayers,new_duration,new_dateAdd,new_currentStatus) => {
-  const targetId = Number(id);
-  for (let game of boardGames) {
-    if (game.id === targetId) {
-      boardGames.splice(boardGames.indexOf(game), 1);
-      game.name = new_name;
-      game.minplayers = new_minplayers;
-      game.maxplayers = new_maxplayers;
-      game.duration = new_duration;
-      game.dateAdd = new_dateAdd;
-      game.currentStatus = new_currentStatus;
-      boardGames.push(game);
-      return game;
-    }
-}
-return null;
-}*/
+
 
 boardGamesService.deleteBoardGame = (id) => {
   const targetId = Number(id);
